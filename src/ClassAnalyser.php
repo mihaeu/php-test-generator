8fbcb300b550770e85a36cfb3453dd50a0011b4b
Michael Haeuslmann <haeuslmann@gmail.com>
2017-07-06 20:50:39 +0200

    Extract constant for constructor name

--- src/ClassAnalyser.php
+++ src/ClassAnalyser.php
@@ -9,12 +9,14 @@ use PhpParser\NodeVisitorAbstract;
 
 class ClassAnalyser extends NodeVisitorAbstract
 {
+    const CONSTRUCTOR_METHOD_NAME = '__construct';
+
     /** @var array */
     private $parameters = [];
 
     public function enterNode(Node $node)
     {
-        if ($node instanceof ClassMethod && $node->name === '__construct') {
+        if ($node instanceof ClassMethod && $node->name === self::CONSTRUCTOR_METHOD_NAME) {
             $this->parameters = array_reduce($node->getParams(), function (array $parameters, Param $parameter) {
                 $parameters[$parameter->name] = $parameter->type->toString();
                 return $parameters;
